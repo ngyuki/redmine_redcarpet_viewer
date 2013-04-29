f20fe82bbb2e84e1337406762a6d8ab41d2d72d0
tsyk goto <ngyuki.ts@gmail.com>
2013-04-29 18:02:06 +0900

    ruby 1.9 support

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-ref ... https://github.com/tmtm/redmine_markdown_extra_viewer
-ref ... https://github.com/alminium/redmine_redcarpet_formatter
+# Redmine Redcarpet Repository Viewer plugin #
+
+## 概要 ##
+
+Redmine のリポジトリビュワーで Markdown ファイルを表示したときに、
+Redcarpet で解析して表示するプラグインです。
+
+
+## インストール ##
+
+Redmine のプラグインディレクトリに移動します。
+
+```console
+$ cd /path/to/redmine/plugins/
+```
+
+プラグインを clone します。
+
+```console
+$ git clone https://github.com/ngyuki/redmine_redcarpet_viewer.git
+```
+
+Bundler で必要なパッケージをインストールします。
+
+```console
+$ bundle install
+```
+
+Redmine を再起動します。
---
+++ lib/repositories_controller_hook.rb
@@ -0,0 +1,31 @@
+require 'redmine'
+
+module RepositoriesControllerHook
+
+  def self.included(base)
+    RepositoriesController.class_eval do
+
+      alias redmine_redcarpet_viewer_orig_entry entry
+      def entry
+        redmine_redcarpet_viewer_orig_entry
+        if not performed? and @path =~ /\.(md|markdown)\z/
+          @content = ::Redmine::MarkdownRedcarpet.to_html(@content)
+          @content.force_encoding('UTF-8') if @content.respond_to?(:force_encoding)
+          render :action => "entry_markdown"
+        end
+      rescue => e
+        @error = e.message
+        render :action => "entry_markdown_error"
+      end
+
+      alias redmine_redcarpet_viewer_orig_is_entry_text_data? is_entry_text_data?
+      def is_entry_text_data?(ent, path)
+        return true if path =~ /\.(md|markdown)\z/
+        redmine_redcarpet_viewer_orig_is_entry_text_data?(ent, path)
+      end
+
+    end
+  end
+end
+
+RepositoriesController.send(:include, RepositoriesControllerHook) unless RepositoriesController.included_modules.include? RepositoriesControllerHook
